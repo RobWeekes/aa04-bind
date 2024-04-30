3ef698541d341eb4e1a5cfc1eefc39b4f6967e19
robweekes <weekes.robert@gmail.com>
2024-04-29 18:12:20 -0700

    done with practice 1

--- employee.js
+++ employee.js
@@ -1,1 +1,23 @@
-// Your code here 
+class Employee {
+    constructor(name, occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    // INSTANCE methods
+    sayName() {
+        console.log(`${this.name} says hello`);
+    }
+
+    sayOccupation() {
+        console.log(`${this.name} is a ${this.occupation}`);
+    }
+
+
+    // CLASS methods
+}
+
+
+// export the Employee class from the file
+
+module.exports = Employee
--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-// Your code here 
+// Import the `Employee` class.
+const Employee = require('./employee');
+
+// Create a new `Employee` with the `name` of "John Wick" , `occupation` of
+// "Dog Lover".
+
+john = new Employee('John Wick', 'Dog Lover');
+console.log(john);
+
+// setTimeout(john.sayName, 2000);     // "undefined says hello"
+
+// Solve the problem using `bind` to make sure that "John Wick says hello" will
+// will be printed to the terminal after 2 seconds.
+
+const johnHello = john.sayName.bind(john);
+global.setTimeout(johnHello, 2000);
+
+// setTimeout(john.sayOccupation, 3000);    // "undefined is a undefined"
+
+const johnJob = john.sayOccupation.bind(john);
+global.setTimeout(johnJob, 3000);
